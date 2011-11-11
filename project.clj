aa59f38388ae49306c9e30625897e34b01b56c01
Phil Yeeles <phil@yeel.es>
2011-11-11 20:17:47 +0000

    added logbook, read-qset, eval-circle

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject nico "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject nico "0.0.1-SNAPSHOT"
+  :description "An Environment for Mathematical Expression in Schools"
   :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :repl-options [:init nil :caught clj-stacktrace.repl/pst+]
   :main nico.core)
--- src/nico/core.clj
+++ src/nico/core.clj
@@ -1,4 +1,17 @@
 (ns nico.core)
 
+(defn read-qset [qsfile]
+  "Reads a set of questions from the file qsfile into a list"
+  (reverse
+   (into ()
+    (clojure.string/split-lines
+     (slurp qsfile)))
+
+(defn eval-circle [circ]
+  "Evaluates the expression represented by the circle circ"
+  (eval
+   (cons (:fn circ)
+         (:args circ))))
+
 (defn -main [& args]
   (prn "Hello, world!"))

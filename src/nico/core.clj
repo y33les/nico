fc428e0aac08c1f416c0e29de148e35dcf2e9d9f
Phil Yeeles <phil@yeel.es>
2011-11-14 12:28:54 +0000

    made notes, comments, need recursive eval-circle

--- src/nico/core.clj
+++ src/nico/core.clj
@@ -7,6 +7,9 @@
     (clojure.string/split-lines
      (slurp qsfile)))))
 
+
+;; fix this, need recursive fn that gens e.g. (+ (* (- 3 2 1) 4) (+ 5 6)) for
+;; several-deep circle maps
 (defn eval-circle [circ]
   "Evaluates the expression represented by the circle circ"
   (eval

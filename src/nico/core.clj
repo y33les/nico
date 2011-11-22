3bd191fe58434c5065f86ae06933c8ba7ec1c8d0
Phil Yeeles <phil@yeel.es>
2011-11-22 12:21:37 +0000

    wrote circ-arg; converts circles to lists, leaves ints as ints

--- src/nico/core.clj
+++ src/nico/core.clj
@@ -10,11 +10,12 @@
 
 ;; fix this, need recursive fn that gens e.g. (+ (* (- 3 2 1) 4) (+ 5 6)) for
 ;; several-deep circle maps
-(defn eval-circle [circ]
+(defn circ-arg [circ]
   "Evaluates the expression represented by the circle circ"
-  (eval
-   (cons (:fn circ)
-         (:args circ))))
+  (cond (= (class circ) clojure.lang.PersistentArrayMap)
+        (cons (:fn circ)
+              (:args circ))
+        :else circ))
 
 (defn -main [& args]
   (prn "Hello, world!"))

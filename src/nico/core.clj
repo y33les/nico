5f45682f2aafd999b6e52c58e7dc26c5721ed117
Phil Yeeles <phil@yeel.es>
2012-02-17 17:00:42 +0000

    fixed docstrings

--- src/nico/core.clj
+++ src/nico/core.clj
@@ -1,25 +1,26 @@
 ;; Nico: An Environment for Mathematical Expression in Schools
 ;; Copyright (C) 2011-2012  Philip M. Yeeles
-;; 
+;;
 ;; This file is part of Nico.
-;; 
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (ns nico.core
   (:gen-class)
   (:use (seesaw core graphics chooser)
-        [clojure.string :only [split split-lines]]))
+        [clojure.string :only [split split-lines]])
+  (:import (java.awt RenderingHints)))
 
 (def screen-x
   ;; Available screen width.
@@ -41,16 +42,19 @@
   ;; Agent containing an integer corresponding to the quesiton number.
   (agent 0))
 
-(defn kill-used-circles []
+(defn kill-used-circles
   "Empties used-circles.  For use in debugging; should be removed from finished program."
+  []
   (def used-circles (agent '())))
 
-(defn kill-current-qset []
+(defn kill-current-qset
   "Empties current-qset.  For use in debugging; should be removed from finished program."
+  []
   (def current-qset (agent '())))
 
-(defn lisp-to-maths [sexp]
+(defn lisp-to-maths
   "Translates a sexp into a string of child-readable maths."
+  [sexp]
   (loop [s   (rest sexp)
          op  (cond (= (eval (first sexp)) +) "+"
                    (= (eval (first sexp)) -) "-"
@@ -64,16 +68,18 @@
           (list? (first s)) (recur (rest s) op (str out "(" (lisp-to-maths (first s)) ")") (inc n))
           :else (recur (rest s) op (str out (first s)) (inc n)))))
 
-(defn find-circle [name]
+(defn find-circle
   "Returns the circle in used-circles with the :name property corresponding to name."
+  [name]
   (loop [n name
          u @used-circles]
     (cond (empty? u) (alert "Circle not found.")
           (= n (:name (first u))) (first u)
           :else (recur n (rest u)))))
 
-(defn read-qset [qsfile]
+(defn read-qset
   "Reads a set of questions from the file qsfile into a list of maps."
+  [qsfile]
   (loop [in (reverse (into () (clojure.string/split-lines (slurp qsfile))))
          out '()
          n 1]
@@ -90,24 +96,27 @@
                   out)
                  (inc n)))))
 
-(defn get-q-no [num]
+(defn get-q-no
   "Gets the question from the current set with the :n field of value num."
+  [num]
   (loop [qs @current-qset
          n  num]
     (cond (empty? qs) nil
           (= n (:n (first qs))) (first qs)
-          :else (recur (rest qs) n))))    
+          :else (recur (rest qs) n))))
 
-(defn nested? [circ]
+(defn nested?
   "Returns true if a circle contains other circles."
+  [circ]
   (cond (not (map? circ)) false
         :else (loop [c (:circ circ)]
                 (cond (empty? c) false
                       (symbol? (first c)) true
                       :else (recur (rest c))))))
 
-(defn eval-circle [circ]
+(defn eval-circle
   "Iterates across a circle list, resolving symbols into their respective circles."
+  [circ]
   (loop [c (:circ circ)
          out '()]
     (cond (empty? c) (reverse out)
@@ -117,8 +126,9 @@
 
 (def main-window)
 
-(defn in-circle [x x?]
+(defn in-circle
   "Checks if the supplied co-ordinate is within a circle (checks against :x if x? is true, :y otherwise) and, if so, returns the name of that circle as a string (otherwise returns nil)."
+  [x x?]
   (loop [u @used-circles]
     (cond x? (cond (empty? u) nil
                    (and (> x (:x (first u)))
@@ -129,12 +139,14 @@
                            (< x (+ (:y (first u)) 100))) (:name (first u))
                       :else (recur (rest u))))))
 
-(defn in-circle? [x x?]
+(defn in-circle?
   "Wrapper function for in-circle that returns true if x is within the horizontal (if x? is true, vertical if x? is false) range of any circle and false otherwise."
+  [x x?]
   (not (nil? (in-circle x x?))))
 
-(defn point-in-circle [x y]
+(defn point-in-circle
   "Checks if the supplied co-ordinates are within a circle and, if so, returns the name of that circle (else nil)."
+  [x y]
   (loop [u @used-circles]
     (cond (empty? u) nil
           (and (> x (:x (first u)))
@@ -143,14 +155,16 @@
                (< y (+ (:y (first u)) 100))) (:name (first u))
           :else (recur (rest u)))))
 
-(defn available? [x x?]
+(defn available?
   "Wrapper function for in-circle? that also checks if the generated co-ordinate will be out of bounds.  If x? is true it checks a x co-ordinate, if not it checks a y co-ordinate."
+  [x x?]
   (cond x? (not (or (in-circle? x x?) (> (+ x 100) screen-x) (in-circle (+ x 100) x?)))
         :else (not (or (in-circle? x x?) (> (+ x 100) screen-y) (in-circle (+ x 100) x?)))))
 
 (comment
-(defn count-nested [circ]
+(defn count-nested
   "Returns an integer corresponding to how many nests of circles a given circle contains."
+  [circ]
   (loop [c (rest (:circ circ))
          n 1]
     (cond (empty? c) n
@@ -160,24 +174,27 @@
           :else (recur (rest c) n))))
 )
 
-(defn leaf? [circ]
+(defn leaf?
   "Returns true if the circle does not have any other circles as arguments."
+  [circ]
   (loop [c (rest (:circ circ))
          l true]
     (cond (empty? c) l
           (symbol? (first c)) false
           :else (recur (rest c) l))))
 
-(defn nested-circles [circ]
+(defn nested-circles
   "Returns a list of circ's arguments that are other circles."
+  [circ]
   (loop [in  (rest (:circ circ))
          out '()]
     (cond (empty? in) (reverse out)
           (symbol? (first in)) (recur (rest in) (cons (str (first in)) out))
           :else (recur (rest in) out))))
 
-(defn is-arg? [circ1 circ2]
+(defn is-arg?
   "Returns true if circ1 is an argument of circ2."
+  [circ1 circ2]
   (loop [n (:name circ1)
          c (rest (:circ circ2))
          a false]
@@ -185,8 +202,9 @@
           (= n (str (first c))) true
           :else (recur n (rest c) a))))
 
-(defn root? [circ]
+(defn root?
   "Returns true if circ is not used as an argument to any other circle."
+  [circ]
   (loop [c circ
          u @used-circles
          r true]
@@ -194,20 +212,23 @@
           (is-arg? c (first u)) false
           :else (recur c (rest u) r))))
 
-(defn find-root []
+(defn find-root
   "Returns the root circle (i.e. the circle to be evaluated that contains all others)."
+  []
   (loop [u @used-circles
          c nil]
     (cond (empty? u) c
           (root? (first u)) (first u)
           :else (recur (rest u) c))))
 
-(defn find-longest-path [c]
+(defn find-longest-path
   "Find the longest path to the circle c."
-  '())
+  [c]
+  [])
 
-(defn count-nested [circ]
+(defn count-nested
   "Returns an integer representing the number of levels traversed before hitting a root node."
+  [circ]
   (loop [c (rest (:circ circ))
          ;; s is number of symbols in current circle
          s (loop [cp c
@@ -221,8 +242,9 @@
                                     :else (recur (rest c) s (+ n (count-nested (find-circle (str (first c))))))
           :else (recur (rest c) s n)))))
 
-(defn link-circles [circ]
+(defn link-circles
   "Draws a lines from a nested circle to its circle-valued arguments."
+  [circ]
   (loop [c (rest (:circ circ))
          x (+ (:x circ) 50)
          y (+ (:y circ) 50)]
@@ -235,8 +257,9 @@
                                   (recur (rest c) x y))
           :else (recur (rest c) x y))))
 
-(defn draw-args [g args x y]
+(defn draw-args
   "Draws the arguments of an expression around its circle."
+  [g args x y]
   (cond (= (count args) 2) (doto g
                              (.drawString (str (nth args 0)) (+ x 48) (+ y 21))
                              (.drawString (str (nth args 1)) (+ x 48) (+ y 89)))
@@ -281,8 +304,9 @@
                              (.drawString (str (nth args 7)) (+ x 23) (+ y 32)))
         :else nil))
 
-(defn draw-circle [circ]
+(defn draw-circle
   "Draws a circle circ at co-ordinates ((:x circ),(:y circ)) on the canvas."
+  [circ]
   (let [g (.getGraphics (select main-window [:#canvas]))
         c circ
         x (:x circ)
@@ -309,22 +333,25 @@
       (draw-args g args x y)
       (link-circles c))))
 
-(defn clear-screen []
+(defn clear-screen
   "Clears all visible drawings from the canvas."
+  []
   (doto (.getGraphics (select main-window [:#canvas]))
     (.setColor java.awt.Color/WHITE)
     (.fillRect 15 15 (- screen-x 30) (- screen-y 30))))
 
-(defn render []
+(defn render
   "Clears the screen, then draws all circles currently in used-circles, linking nested circles together."
+  []
   (do
     (clear-screen)
     (loop [u @used-circles]
       (cond (not (empty? u)) (do (draw-circle (first u))
                                  (recur (rest u)))))))
 
-(defn load-qset-init []
+(defn load-qset-init
   "Brings up a dialogue with a file chooser to specify where to load the question set from.  Sends off the contents of the chosen file to current-qset."
+  []
   (choose-file :filters [(file-filter "Nico Question Set (*.nqs)"
                                       #(or (.isDirectory %)
                                            (=
@@ -338,9 +365,10 @@
                                             (.getAbsoluteFile f))))
                                (send-off current-question
                                          (fn [_] 1))))))
-                         
-(defn load-qset [& a]
+
+(defn load-qset
   "Brings up a dialogue with a file chooser to specify where to load the question set from.  Sends off the contents of the chosen file to current-qset."
+  [& a]
   (choose-file :filters [(file-filter "Nico Question Set (*.nqs)"
                                       #(or (.isDirectory %)
                                            (=
@@ -365,8 +393,9 @@
                                (kill-used-circles)
                                (render)))))
 
-(defn gen-circ-radios [pfx gp]
+(defn gen-circ-radios
   "Returns a horizontal-panel containing one radio button for each circle currently in used-circles.  Each radio button has the :id :(str pfx :name), and is in the button-group gp."
+  [pfx gp]
   (horizontal-panel :id (keyword (str pfx "-circpanel"))
                     :items (loop [in  @used-circles
                                       out '()
@@ -384,8 +413,9 @@
 ;; Groups of radio buttons for use in new-dialogue
 (def op (button-group))
 
-(defn new-arg-panel [n]
+(defn new-arg-panel
   "Creates the configuration panel for argument n to be used in new-dialogue."
+  [n]
   (let [g  (button-group)
         gr (button-group)]
   (border-panel :id (keyword (str "args-selector-" n))
@@ -413,8 +443,9 @@
 
 (def new-circle) ;; Declare new-circle, to be defined later
 
-(defn new-dialogue []
+(defn new-dialogue
   "Generates the dialogue to be displayed as part of new-circle."
+  []
   (do
     (native!)
     (border-panel :id :new-box
@@ -448,8 +479,9 @@
                                                  (new-arg-panel 6)
                                                  (new-arg-panel 8)]))))
 
-(defn re-eval-box []
+(defn re-eval-box
   "Regenerate the bits of new-dialogue that need regenerating."
+  []
   (do
     (config! (select new-dialogue [:#args-left]) :items [(new-arg-panel 1)
                                                          (new-arg-panel 3)
@@ -462,8 +494,9 @@
 
 (def check-answer) ;; Declare check-answer, to be defined later
 
-(defn new-circle [x y]
+(defn new-circle
   "Brings up a dialogue box to configure a new circle.  Draws the circle on pressing 'OK'."
+  [x y]
   (let [dlg (new-dialogue)]
     (invoke-later
      (-> (dialog :title "New",
@@ -512,8 +545,9 @@
           pack!
           show!))))
 
-(defn del-circle [x y & a]
+(defn del-circle
   "Removes a circle from used-circles, such that it won't reappear on executing render."
+  [x y & a]
   (do
     (send-off used-circles (fn [_] (loop [in @used-circles
                                           out '()
@@ -526,8 +560,9 @@
     (render)
     (check-answer)))
 
-(defn next-question []
+(defn next-question
   "Loads the next question in the current question set."
+  []
   (do
     (send-off current-question #(inc %))
     (await current-question)
@@ -544,8 +579,9 @@
                   (alert "Well done!  Please choose another question set.")
                   (load-qset)))))
 
-(defn question-right []
+(defn question-right
   "To be executed when a question is answered correctly."
+  []
   (do
     (config!
      (select main-window [:#answer])
@@ -557,15 +593,17 @@
     (await used-circles)
     (render)))
 
-(defn question-wrong []
+(defn question-wrong
   "To be executed when a question is answered incorrectly."
+  []
   (config!
    (select main-window [:#answer])
    :text (str (eval (eval-circle (find-root))))
    :foreground "#FF0000"))
 
-(defn check-answer []
+(defn check-answer
   "Evaluate the current root circle and check against the answer to the current question, displaying the result to the user."
+  []
   (cond (empty? @used-circles) (config! (select main-window [:#answer]) :text (str 0))
         (= (eval (eval-circle (find-root))) (eval (eval (:q (get-q-no @current-question))))) (question-right)
         :else (question-wrong)))
@@ -620,7 +658,7 @@
 Copyright " \u00a9 " 2011-2012 Philip M. Yeeles\n
 Nico is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
+the XFree Software Foundation, either version 3 of the License, or
 (at your option) any later version.\n
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -631,7 +669,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.")))
           :mnemonic \A
           :name "About..."))
 
-(defn -main [& args]
+(defn -main
+  "Main method, initialises application."
+  [& args]
   (do
     (native!)
     (load-qset-init)
@@ -648,4 +688,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.")))
                   :on-close :exit)
            pack!
            show!)
-       (config! (select main-window [:#question]) :text (lisp-to-maths (first (rest (:q (first @current-qset))))) :border (str "Question " (:n (first @current-qset))))))))
+       (doto (.getGraphics (select main-window [:#canvas]))
+         (.setRenderingHint RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON))
+       (config! (select main-window [:#question])
+                :text (lisp-to-maths (first (rest (:q (first @current-qset)))))
+                :border (str "Question " (:n (first @current-qset))))))))
